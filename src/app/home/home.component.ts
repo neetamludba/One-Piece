84613d86a4f19e645ab8bb666355e642fa222a40
Abdul Mateen <abdulmateen7733@gmail.com>
2022-05-04 11:02:37 +0500

    sidenav in layout issue resolved

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,40 +11,40 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  displayVarMarines: boolean = false;
-  marinesDisplay(){
-    if(this.displayVarMarines){
-      this.displayVarMarines = false;
-    }else{
-      this.displayVarMarines = true;
-      this.displayVarPirates = false;
-      this.displayVarRArmy = false;
-    }
+  // displayVarMarines: boolean = false;
+  // marinesDisplay(){
+  //   if(this.displayVarMarines){
+  //     this.displayVarMarines = false;
+  //   }else{
+  //     this.displayVarMarines = true;
+  //     this.displayVarPirates = false;
+  //     this.displayVarRArmy = false;
+  //   }
 
-  }
+  // }
 
-  displayVarPirates: boolean = false;
+  // displayVarPirates: boolean = false;
 
-  piratesDisplay(){
-    if(this.displayVarPirates){
-      this.displayVarPirates = false;
-    }else{
-      this.displayVarMarines = false;
-      this.displayVarPirates = true;
-      this.displayVarRArmy = false;
-    }
-  }
+  // piratesDisplay(){
+  //   if(this.displayVarPirates){
+  //     this.displayVarPirates = false;
+  //   }else{
+  //     this.displayVarMarines = false;
+  //     this.displayVarPirates = true;
+  //     this.displayVarRArmy = false;
+  //   }
+  // }
   
-  displayVarRArmy: boolean = false;  
+  // displayVarRArmy: boolean = false;  
   
-  rArmyDisplay(){
-    if(this.displayVarRArmy){
-      this.displayVarRArmy = false;
-    }else{
-      this.displayVarMarines = false;
-      this.displayVarPirates = false;
-      this.displayVarRArmy = true;
-    }
-  }
+  // rArmyDisplay(){
+  //   if(this.displayVarRArmy){
+  //     this.displayVarRArmy = false;
+  //   }else{
+  //     this.displayVarMarines = false;
+  //     this.displayVarPirates = false;
+  //     this.displayVarRArmy = true;
+  //   }
+  // }
 
 }
